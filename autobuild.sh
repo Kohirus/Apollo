540db2cf509ad543fc105ad7682bde67d1e4f216
leaos <kohirus@foxmail.com>
2023-02-11 20:53:15 +0800

    first commit

---
+++ autobuild.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BUILD_DIR=./build/
+BIN_DIR=./bin/
+LIB_DIR=./lib/
+
+set -x
+
+rm -rf $BUILD_DIR/*
+
+if [ ! -d "$BUILD_DIR" ];then
+  mkdir $BUILD_DIR
+fi
+
+if [ ! -d "$BIN_DIR" ];then
+  mkdir $BIN_DIR
+fi
+
+if [ ! -d "$LIB_DIR" ];then
+  mkdir $LIB_DIR
+fi
+
+cd $BUILD_DIR &&
+  cmake .. &&
+  make
